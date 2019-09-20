1df304559c797dda38293f34a00921570ddac07e
DuyPhong4797 <48857792+DuyPhong4797@users.noreply.github.com>
2019-09-20 15:51:22 +0700

    Update model.php

--- model/model.php
+++ model/model.php
@@ -22,20 +22,6 @@
 			    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
 			}
 			
-			$link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
- 
-			if (!$link) {
-			    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-			    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-			    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-			    exit;
-			}
-
-			echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
-			echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
-
-			mysqli_close($link);
-			die();
 			$this->conn = new mysqli($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword, $connectstr_dbname) or die("Loi ket noi");
 			$this->conn->set_charset("UTF8");
 		}
